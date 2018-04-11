76c79875ef1654638039a8b6d41c1c1737d9db9c
Poroh <meta.poroh.kun@gmail.com>
2018-04-11 15:57:39 +0300

    layers manager

---
+++ Assets/Scripts/GUI/Layers/MainMenuLayer.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using UnityEngine;
+
+namespace Layers
+{
+    public class MainMenuLayer : LayerBase
+    {
+        public override HidingType Hiding { get { return HidingType.HideAll; } }
+
+        public void OnPlay()
+        {
+            LayersManager.FadeOut(0.5f, () =>
+            {
+                //LayersManager.Push<GameLayer>().Initialize();
+                LayersManager.FadeIn(0.5f, () =>
+                {
+                });
+            });
+        }
+
+        public void OnTutorial()
+        {
+            //LayersManager.Push<GameLayer>();
+        }
+        
+        public void OnFeedback()
+        {
+            //LayersManager.Push<FeedbackLayer>();
+        }
+    }
+}
---
+++ Assets/Scripts/GUI/Layers/StartLayer.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections;
+using UnityEngine;
+
+namespace Layers
+{
+    public class StartLayer : LayerBase
+    {
+        void Start()
+        {
+            LayersManager.Push<MainMenuLayer>();
+            LayersManager.FadeIn(0f, null);
+        }
+
+    }
+}
